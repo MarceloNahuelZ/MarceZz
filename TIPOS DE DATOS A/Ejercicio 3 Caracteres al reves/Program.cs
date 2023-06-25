b214a4a9d978e9affd98501c951d2f04252ce600
MarceloNahuelZ <marcelo.zabaletta@gmail.com>
2023-06-25 14:33:05 -0300

    Primer Commit

---
+++ TIPOS DE DATOS A/Ejercicio 3 Caracteres al reves/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Escriba un programa en C# que solicite al usuario tres letras y los muestre al revés.
+
+namespace Ejercicio_3_Caracteres_al_reves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caracteres al revés");
+
+            Console.WriteLine("Ingrese la primera letra:");
+            char letra1 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la segunda letra:");
+            char letra2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la tercera letra:");
+            char letra3 = Convert.ToChar(Console.ReadLine());
+
+            string resultado = letra3.ToString() + letra2.ToString() + letra1.ToString();
+            Console.WriteLine("Las letras al revés son: " + resultado;
+        }
+    }
+}
---
+++ TIPOS DE DATOS A/Ejercicio 4 Centigrados a Kelvin y Fahrenheit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*La escala Celsius es centígrada, 100 divisiones separan el punto de congelación del punto de ebullición del agua. 
+ * En la escala Fahrenheit de los anglosajones, estos dos puntos están separados por 180 grados. 
+ * La escala de Kelvin es una escala absoluta utilizada en ciencias.
+
+Cree un programa en C# para convertir de grados centígrados a Kelvin y Fahrenheit. 
+Solicite al usuario la cantidad de grados centígrados para convertirlos usando las siguientes tablas de conversión:
+
+kelvin = celsius + 273
+fahrenheit = celsius x 18/10 + 32
+*/
+
+namespace Ejercicio_4_Centigrados_a_Kelvin_y_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Centigrados a Kelvin y Fahrenheit..");
+
+            Console.WriteLine("Ingrese los Grados Centigrados");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double kelvin = celsius + 273;
+            double fahrenheit = celsius * 18 / 10  + 32;
+
+            Console.WriteLine("sus grados en kelvin son:" + kelvin );
+
+            Console.WriteLine("sus grados en fahrenheit son:" + fahrenheit);
+
+
+        }
+    }
+}
