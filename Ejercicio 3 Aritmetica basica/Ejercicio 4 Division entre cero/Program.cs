2bef27b5f594bdf5b5ab081f8bf6d3ffffaaa03d
MarceloNahuelZ <marcelo.zabaletta@gmail.com>
2023-06-26 14:45:02 -0300

    actualiza repositorio
    prueba primer repositorio git

---
+++ Ejercicio 3 Aritmetica basica/Ejercicio 4 Division entre cero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * Cree un programa en C# que solicite dos números al usuario (x, y)
+ * y muestre el resultado de su división.
+
+ Tendrá que controlar la división por 0 con el uso de la excepción DivideByZeroException
+ en un bloque try-catch. Si el usuario intenta dividir por 0 muestre No se puede dividir por 0.
+*/
+
+
+namespace Ejercicio_4_Division_entre_cero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divion entre 0 ");
+            int x = Int32.TryParse(Console.ReadLine());
+            
+            
+
+        }
+    }
+}
---
+++ Ejercicio 3 Aritmetica basica/Ejercicio 4 Orden de prioridad/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Crea un programa en C# que solicite un número entero x al usuario y
+* imprima el resultado de las siguientes operaciones:
+* -6 + x * 5
+* (13-2) * x
+* (x + -2) * (20 / 10)
+* (12 + x) / (5 - 4)
+*/
+
+namespace Ejercicio_4_Orden_de_prioridad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Orden de prioridad ");
+
+            Console.Write("Ingrese un numero entero: ");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int ejer1 = -6 + x * 5;
+            int ejer2 = (13 - 2) * x;
+            int ejer3 = (x + -2) * (20 / 10);
+            int ejer4 = (12 + x) / (5 - 4);
+
+            Console.WriteLine("Resultado 1: " +  ejer1);
+            Console.WriteLine("Resultado 2: " +  ejer2);
+            Console.WriteLine("Resultado 3: " +  ejer3);
+            Console.WriteLine("Resultado 4: " +  ejer4);
+
+        }
+    }
+}
