3b0a0abe14dd808937ceb31e2f647ec7a021210f
MarceloNahuelZ <marcelo.zabaletta@gmail.com>
2023-07-15 12:08:25 -0300

    wF

---
+++ Condiciones b/Tablas multiplicar/Tablas multiplicar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Cree un programa en C# que solicite un rango de números al usuario (x,y) 
+ * y muestre las tablas de multiplicar de x hasta y. */
+
+namespace Tablas_multiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            Console.WriteLine("Ingrese el primer numero:  ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su segundo numero: ");
+            y = Convert.ToInt32(Console.ReadLine());    
+
+            for (int i = x ; i <= y; i++)
+            {
+                for (int j = 1 ; j <= 10; j++)
+                {
+                    Console.WriteLine(i + "X" + j + "=" + (i*j) + " ");
+                }
+                Console.WriteLine();
+            }
+           
+            }
+        }
+    }
+
---
+++ Winsform tp7/PrubedeFunciones/PrubedeFunciones/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrubedeFunciones
+{
+    static void SumaPorValor(int a, int b, int resultado)
+    {
+        resultado = a + b;
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
